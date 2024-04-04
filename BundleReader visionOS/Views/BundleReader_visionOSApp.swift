2dfa6558e1c39c61aad0852b9a64178b2856dec5
Rayan Khan <iRayanKhan@icloud.com>
2024-04-04 13:35:42 -0500

    - Add visionOS app

---
+++ BundleReader visionOS/Views/BundleReader_visionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BundleReader_visionOSApp.swift
+//  BundleReader visionOS
+//
+//  Created by Rayan Khan on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BundleReader_visionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

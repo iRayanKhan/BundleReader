9b49fc7e3e2f988732ae318d1a93d0e3dabaa6f3
Rayan Khan <iRayanKhan@icloud.com>
2024-04-04 12:41:34 -0500

    Initial Commit

---
+++ BundleReader/BundleReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BundleReaderApp.swift
+//  BundleReader
+//
+//  Created by Rayan Khan on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BundleReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BundleReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BundleReader
+//
+//  Created by Rayan Khan on 4/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
